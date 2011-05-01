04026cfca626607115a0b448549ae6a4986accdc
Andrew Schaaf <andrew@andrewschaaf.com>
2011-05-01 02:06:59 -0400

    fix

--- src/com/google/bitcoin/core/TransactionStandaloneEncoder.java
+++ src/com/google/bitcoin/core/TransactionStandaloneEncoder.java
@@ -51,35 +51,45 @@ public class TransactionStandaloneEncoder {
             wallet.addKey(in_keys.get(i));
         }
 
-        Transaction sendTx = new Transaction(params);
-
-        // Outputs
-        for (int i = 0; i < out_amounts.size(); i++) {
-            sendTx.addOutput(new TransactionOutput(params, out_amounts.get(i), out_addresses.get(i), sendTx));
-        }
-
-        // Inputs
-        Transaction t_in;
+        // Prereq transactions
+        ArrayList<TransactionOutput> in_outputs = new ArrayList<TransactionOutput>();
+        Transaction t;
+        TransactionOutput output;
         Address whatever = new ECKey().toAddress(params);
         for (int i = 0; i < in_keys.size(); i++) {
 
-            t_in = new Transaction(params);
+            t = new Transaction(params);
 
             // index (via spacers)
             int index = in_indexes.get(i).intValue();
             for (int j = 0; j < index; j++) {
-                t_in.addOutput(new TransactionOutput(params, Utils.toNanoCoins(1, 0), whatever, t_in));
+                t.addOutput(new TransactionOutput(params, Utils.toNanoCoins(1, 0), whatever, t));
             }
+            output = new TransactionOutput(params, Utils.toNanoCoins(1, 0), in_keys.get(i).toAddress(params), t);
+            t.addOutput(output);
+            in_outputs.add(output);
 
             // hash
-            t_in.setFakeHashForTesting(new Sha256Hash(in_hashes.get(i)));
+            t.setFakeHashForTesting(new Sha256Hash(in_hashes.get(i)));
 
             try {
-                wallet.receive(t_in, null, BlockChain.NewBlockType.BEST_CHAIN);
+                wallet.receive(t, null, BlockChain.NewBlockType.BEST_CHAIN);
             }
             catch (Exception e) {
                 throw new RuntimeException(e);
             }
+        }
+
+        Transaction sendTx = new Transaction(params);
+
+        // Inputs
+        for (int i = 0; i < in_outputs.size(); i++) {
+            sendTx.addInput(in_outputs.get(i));
+        }
+
+        // Outputs
+        for (int i = 0; i < out_amounts.size(); i++) {
+            sendTx.addOutput(new TransactionOutput(params, out_amounts.get(i), out_addresses.get(i), sendTx));
         }
 
         try {
